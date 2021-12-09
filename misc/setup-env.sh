8e88c2ab3dfe78039f289e13c6018f7d81649ecb
Stefan Fürcho <l.fuercho@avm.de>
2021-12-09 15:53:09 +0100

    Updated scripts to use regular flutter channel
    - Web functionality is no longer only in the beta channel
    - Changed flutter path to default location when using the snap image

--- misc/setup-env.sh
+++ misc/setup-env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export ANDROID_SDK_ROOT=${HOME}/bin/android
-export FLUTTER_PATH=${HOME}/bin/flutter/bin
+#export ANDROID_SDK_ROOT=${HOME}/bin/android
+export FLUTTER_PATH=${HOME}/snap/flutter/common/flutter/.pub-cache/bin
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 export PATH=${JAVA_HOME}/bin:${FLUTTER_PATH}:${PATH}
-export PATH="$PATH":"$HOME/bin/flutter/.pub-cache/bin"
